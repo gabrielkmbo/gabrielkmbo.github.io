9ae21df8ac94402ec7b8ee1aad0fbf967b11ec36
Gabriel Bo <gabrielkmbo@gmail.com>
2024-12-31 22:44:27 -0600

    reformatting

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/gabrielkmbo/github.io/', // Replace with your repository name
-});
